109a890536c2403dde39eedd91bb32058646fa2d
pilipb <xq19351@bristol.ac.uk>
2024-01-06 14:30:29 +0000

    Update contour.py

--- contour.py
+++ contour.py
@@ -1,29 +1,37 @@
-import cv2
+import scipy
+import matplotlib.pyplot as plt
+import numpy as np
+import imageio
 
-# Load the input image
-image = cv2.imread('colour.jpeg')
+# make a voronoi centroid map for an image
+def voronoi_centroids(img, n):
+    # find the centroids of the image
+    centroids = scipy.ndimage.measurements.center_of_mass(img, img, range(1, n+1))
+    # make a voronoi diagram
+    vor = scipy.spatial.Voronoi(centroids)
+    # return the voronoi diagram and the centroids
+    return vor, centroids
 
-# Convert the image to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# make a voronoi centroid map for waves.jpg
+img = im = imageio.imread('waves.jpg')
+vor, centroids = voronoi_centroids(img, 10)
+print(centroids[0][:][0])
+# plot the voronoi diagram
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.imshow(img)
+# for cen in centroids:
+#     ax.plot(cen[0], cen[1], 'r.')
+for pointidx, simplex in zip(vor.ridge_points, vor.ridge_vertices):
+    simplex = np.asarray(simplex)
+    if np.all(simplex >= 0):
+        ax.plot(vor.vertices[simplex, 0], vor.vertices[simplex, 1], 'k-')
+center = vor.points.mean(axis=0)
+ax.plot(vor.vertices[:,0], vor.vertices[:,1], 'go')
 
-ret,thresh = cv2.threshold(gray,27,25,0)
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_TC89_L1)
-print(type(contours)) #type list
-
-c = max(contours, key=cv2.contourArea) #max contour
-f = open('path.svg', 'w+')
-f.write('<svg width="'+str(100)+'" height="'+str(100)+'" xmlns="http://www.w3.org/2000/svg">')
-f.write('<path d="M')
-
-for i in range(len(c)):
-    #print(c[i][0])
-    x, y = c[i][0]
-    # print(x)
-    f.write(str(x)+  ' ' + str(y)+' ')
-
-f.write('"/>')
-f.write('</svg>')
-f.close()
-
-
-
+    # ax.plot(vor.vertices[:,0], vor.vertices[:,1], 'go')
+# for region in vor.regions:
+#     if not -1 in region:
+#         polygon = [vor.vertices[i] for i in region]
+#         ax.plot(*zip(*polygon), color='b')
+plt.show()
